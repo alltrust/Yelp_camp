4f494f2658c2697f81c2b14468994ecfda5ab3e2
Aldo Garcia <aldorgarcia29@gmail.com>
2021-11-23 19:50:24 -0500

    add start script

--- index.js
+++ index.js
@@ -165,6 +165,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err }) //error is that name of the error.ejs file
 })
 
-app.listen(3000, () => {
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
     console.log("PORT 3000")
 })
